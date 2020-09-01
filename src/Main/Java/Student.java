6d75748d6d4fd6866aae0c8b77f83438f86d016b
Donald Spini <donald.spini68@gmail.com>
2020-09-01 13:34:22 -0500

    Student

---
+++ src/Main/Java/Student.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+
+
+public class Student {
+
+//  `id` should be a `long` number used to represent a "unique user" in our application.
+//  `name` is a `String` that holds the name of the student.
+//  `grades` is an `ArrayList` that contains a list of `Integer` numbers.
+
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+//  The `Student` class should have a constructor that sets both the name and id property,
+//  it initializes the grades property as an empty ArrayList.
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+//  The `Student` class should have the following methods:
+
+    public long getId() {
+        return this.id; // returns the student's id
+    }
+
+    public String getName() {
+        return this.name; // returns the student's name
+    }
+
+    public ArrayList<Integer> getGrades(){
+        return this.grades; // adds the given grade to the grades list
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade); // returns the average of the students grades
+    }
+
+    public double getAverage() {
+
+        if(this.grades.size() == 0){
+            return 0;
+        }
+
+        double sum = 0;
+        for (int grade: this.grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size();
+    }
+}
---
+++ src/Test/Java/StudentTest.java
@@ -0,0 +1,55 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+//  Create a new branch called `students-tests`
+public class StudentTest {
+
+    Student don;
+    Student spini;
+
+    @Before
+    public void init(){
+        don = new Student(1L, "Don");
+        spini = new Student(2L, "Spini");
+
+    }
+
+    @Test
+    public void testIfIdWorks(){
+        assertEquals(1L, don.getId());
+        assertEquals(2L, spini.getId());
+    }
+
+    @Test
+    public void testIfNameWorks(){
+        assertEquals("Don", don.getName());
+        assertEquals("Spini", spini.getName());
+        assertNotEquals("Don", spini.getName());
+    }
+
+    @Test
+    public void testIfGradesWorks(){
+        assertEquals(0, don.getGrades().size());
+    }
+
+    @Test
+    public void testIfAddGradeWorks(){
+        don.addGrade(80);
+        assertTrue(80 == don.getGrades().get(0));
+        don.addGrade(100);
+        assertEquals(100, don.getGrades().get(1), 0);
+    }
+
+    @Test
+    public void testIfAvgWorks(){
+        assertEquals(0, don.getAverage(), 0);
+        don.addGrade(100);
+        don.addGrade(50);
+        don.addGrade(100);
+        assertEquals(83.33, don.getAverage(), 0.1);
+        don.addGrade(50);
+        assertEquals(75, don.getAverage(), 0);
+    }
+
+}
